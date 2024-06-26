955c67ede6148dc1d3f3219dae67cf57f8fef011
omprakash8788 <omprakashkumar552@gmail.com>
2024-06-26 23:17:24 +0530

    test

--- chap14/exc1.rb
+++ chap14/exc1.rb
@@ -1,19 +1,38 @@
-CREATE TABLE person (
-  id INT,
-  name VARCHAR(100),
-  city VARCHAR(100),
+CREATE TABLE table_name(
+  column_name1 datatype constraint
+  column_name2 datatype constraint
+  column_name3 datatype constraint
+
+)
+
+Example
+
+CREATE TABLE customer(
+  "ID" int8 PRIMARY KEY,
+  "Name" varchar(255) NOT NULL,
+  "Age" int NOT NULL,
+   "City" char(255),
+  "Salary" numeric(10,2)"
+
+)
+
+INSERT INTO customer
+(CustID, CustName, Age, City, Salary)
+values
+(1, 'John', 25, 'New York', 50000),
+(2, 'Jane', 30, 'Los Angeles', 60000),
+(3, 'Bob', 35, 'Chicago', 70000),
+(4, 'Alice', 40, 'Houston', 80000)
+
+
+
+
+Creating the Table
+
+CREATE TABLE customer (
+  "ID" int8 PRIMARY KEY,
+  "Name" varchar(255) NOT NULL,
+  "Age" int NOT NULL,
+  "City" varchar(255),
+  "Salary" numeric(10,2)
 );
-CREATE TABLE person (id INT, name VARCHAR(100), city VARCHAR(100))
-
-INSERT INTO person (id, name, city)
-VALUES (101,'omprkash','mumbai');
-INSERT INTO students VALUES (101,'omprakash')
-
-
-SELECT * FROM <table_name>
-SELECT <column_name> from students
-
-
-UPDATE person 
-SET city ='London'
-WHERE id=2;
